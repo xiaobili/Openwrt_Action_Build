d4ebc41d30fe3996260566c78cfc022a6622aa89
BillyJR <zygboom@gmail.com>
2026-01-31 13:32:23 +0800

    fix

--- diy-mini.sh
+++ diy-mini.sh
@@ -30,7 +30,7 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-netspeedtest.git packag
 # 更改Argon 主题背景
 cp -f $GITHUB_WORKSPACE/images/background.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
-ech "当前工作流: $GITHUB_WORKFLOW"
+echo "当前工作流: $GITHUB_WORKFLOW"
 
 # 如果当前 action 为 LEDE
 if [[ $GITHUB_WORKFLOW == *"LEDE"* ]]; then
